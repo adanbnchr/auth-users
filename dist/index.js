c06c0be7cfe5d4ddf7934f6459a99bda6728345f
juanda <juandacorreo@gmail.com>
2017-06-29 16:01:56 +0200

    boilerplate with es6

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// var randtoken = require('rand-token');
+// var nodemailer = require('nodemailer');
+
+var _options = require('./options.js');
+
+var _options2 = _interopRequireDefault(_options);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// var options = require('options.js')
+
+console.log(_options2.default);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// var randtoken = require('rand-token');
+// var nodemailer = require('nodemailer');
+import options from './options.js';
+// var options = require('options.js')
+
+console.log(options);
